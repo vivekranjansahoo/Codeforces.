acb850260b2b7b1b2488a365cb932c450eb7ccfe
vivekranjansahoo <97107034+vivekranjansahoo@users.noreply.github.com>
2022-10-21 20:33:06 +0530

    Create 231A - Team.cpp

---
+++ Pratices/Rating/800/231A - Team.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+
+inline void debugMode() {
+
+
+#ifndef ONLINE_JUDGE
+
+    freopen("input.txt", "r", stdin);
+
+    freopen("output.txt", "w", stdout);
+
+#endif // ONLINE_JUDGE
+}
+
+int main()
+{
+
+    debugMode();
+
+    int n, Petya, Vasya, Tonya, number(0);
+    cin >> n;
+    while (n--)
+    {
+        cin >> Petya >> Vasya >> Tonya;
+        if (Petya + Vasya + Tonya >= 2)
+        {
+            number += 1;
+        }
+    }
+    cout << number << endl;
+    return 0;
+    return 0;
+}
